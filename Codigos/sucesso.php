b187d3de483cb160551a017a52d0501dd78fc67a
Kauã da Silva de Mattos <kauasilvamattos0000@gmail.com>
2024-05-14 18:02:27 -0300

    teste

---
+++ Codigos/sucesso.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sucesso/title>
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+</head>
+<body>
+    <main>
+        <section>
+            <div id="imagem">
+
+            </div>
+            <div id="formulario">
+                <h1>tudo certo</h1>
+                <p>
+                <?php
+                session_start();
+
+                if (!isset($_SESSION['login'])) {
+                    header("Location: login.php");
+                    exit();
+                }
+
+                echo "Bem-vindo, " . $_SESSION['login'];
+                ?>
+                </p>
+                <p>
+                    <form method="get" action="login.php">
+                        <input type="submit" value="Fazer Login">
+                    </form>
+                </p>
+                <p>
+                    <form method="get" action="cadastro.php">
+                        <input type="submit" value="Cadastrar-se">
+                    </form>
+                </p>
+                
+            </div>
+        </section>
+    </main>
+    
+</body>
+</html>
