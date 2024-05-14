a24da331256e690c82f369281aee18580417d5a7
Kauã da Silva de Mattos <kauasilvamattos0000@gmail.com>
2024-05-14 18:11:09 -0300

    login

--- Codigos/sucesso.php
+++ Codigos/sucesso.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sucesso/title>
+    <title>Sucesso</title>
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
 <body>
