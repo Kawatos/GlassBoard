e105634c4d9b172250d156b91576a7fb3bb6d904
Kauã da Silva de Mattos <108145993+Kawatos@users.noreply.github.com>
2024-08-01 17:43:02 -0300

    docker sucesso

--- lib/classes/config.php
+++ lib/classes/config.php
@@ -1,8 +1,11 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
 // config.php
 return [
-    'dbHost' => 'localhost',
-    'dbUser' => 'root',
-    'dbPass' => '',
-    'dbName' => 'glassboard',
+    'dbHost' => '177.52.160.63',
+    'dbUser' => 'acampos_glassbo',
+    'dbPass' => '321654987Om',
+    'dbName' => 'acampos_glassboard',
 ];
